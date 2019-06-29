2cd84e9da291c8c763dc64ed8cd3d9f5bfc0e933
Chris Thurber <cthurber@me.com>
2019-06-29 11:08:17 -0400

    Converted from key:value sections to Section objects

--- Statement.py
+++ Statement.py
@@ -1,28 +1,28 @@
 import re
 import json
 from edgar_utils import load_config
+from Section import Section
+import multiprocessing as mp
 
 class Statement(object):
     """Generally, this object represents a financial statement for a company
     In its current implementation however, this is pretty tightly coupled to a 10-K filing"""
 
-    def __init__(self, type, config, contents):
+    def __init__(self, type, config, contents, processors=1):
         super(Statement, self).__init__()
         self.type = type
         self.contents = contents
         self.year = ""
         self.config = config["statements"]
+        self.processors = processors
 
         # TODO Create array of Section object; multi-thread this
-        self.sections = {self.config["statement_sections"][section]["description"] : self.get_section(section) for section in self.config["statement_sections"]}
+        # self.sections = {}
+        self.sections = []
 
     # TODO - Critical feature... ensure a statement can be tied to a datetime
     def set_year(self):
         self.year = "2019"
-        pass
-
-    def get_sections(self):
-
         pass
 
     def get_section_text(self, section):
@@ -30,6 +30,7 @@ class Statement(object):
         # Gather expressions for removing HTML, spec chars, etc.
         cleaner_expressions = self.config["section_cleaners"]
 
+        # Parallelize
         for cleaner in cleaner_expressions:
             cleaner = cleaner_expressions[cleaner]
             section = re.sub(cleaner["regex"], cleaner["replace"], section)
@@ -37,6 +38,9 @@ class Statement(object):
         return section
 
     def get_section(self, section):
+
+        section_name = self.config["statement_sections"][section]["description"]
+
 
         # Load up the title and regular expression for the requested section
         section_config = self.config["statement_sections"]
@@ -52,6 +56,18 @@ class Statement(object):
             # Clean up any special characters, HTML, etc. from the section
             section_contents = self.get_section_text(raw_section)
 
-            return section_contents
+            return Section(section_name, section_contents)
+
         except:
-            return ""
+            return None
+
+    def get_sections(self):
+
+        # TODO - Parallelize
+        # self.sections = [pool.apply(, args=())]
+        for section in self.config["statement_sections"]:
+
+            new_section = self.get_section(section)
+
+            if new_section:
+                self.sections.append(new_section)
