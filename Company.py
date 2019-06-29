4f19796d084fc43602586472de0f59907fde904c
Chris Thurber <cthurber@me.com>
2019-06-29 10:19:34 -0400

    Updated Company to receive config as reference rather than reading config on each initilization

--- Company.py
+++ Company.py
@@ -8,14 +8,15 @@ from Statement import Statement
 class Company(object):
     """Defines a company which is initiatlized with a name and identifier"""
 
-    def __init__(self, name, symbol=None):
+    def __init__(self, name, config, symbol=None):
         super(Company, self).__init__()
         self.name = name
         self.symbol = symbol
         self.cik = None
         # TODO this should be by year
         self.statements = []   # Array of statement objects
-        self.dir_config = load_config()["directories"]
+        self.config = config
+        self.dir_config = config["directories"]
 
     def cache_content(self):
         # Create directory for the current date
@@ -33,11 +34,16 @@ class Company(object):
     def get_statements(self, cache=True):
 
         # TODO - Enhancement: Should make a 'producer' here that is consumed by the block below
+        # TODO - Create sub-function that can handle retries
+        # TODO - Handle CIK matching
+        if self.cik is None:
+            print("Error: CIK was not initialization for", self.name)
+
         content = [get_content(url, self.name) for url in get_content_urls(self.cik)]
 
         # TODO - Enhancement: As content becomes available, this block should consume it
         for c in content:
-            statement = Statement("10-K", c)
+            statement = Statement("10-K", self.config, c)
             statement.set_year
             self.statements.append(statement)
             # self.statements[statement.year] = statement
