791280c7f69605bc786b678985424210b3c5184e
Chris Thurber <cthurber@me.com>
2019-06-29 10:20:52 -0400

    Create Section object to more effectively store/access sections of a statement (to replace key:value store in Statement class)

---
+++ Section.py
@@ -0,0 +1,8 @@
+
+class Section(object):
+    """docstring for Section."""
+
+    def __init__(self, name, content):
+        super(Section, self).__init__()
+        self.name = name
+        self.content = content
